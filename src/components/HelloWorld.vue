b7eeeb1cda717d5bf90a6f05935ed3c242a05b1b
Maheu <chrisii868@gmail.com>
2019-04-12 10:57:07 -0500

    Fetch data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,97 +1,34 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
-</template>
-
 <script>
+import azure from 'azure-storage'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Loading'
     }
+  },
+
+  created () {
+    const sas = 'sv=2018-03-28&ss=bfqt&srt=sco&sp=rl&se=2019-12-31T05:44:10Z&st=2019-04-07T20:44:10Z&spr=https&sig=0dTdKw%2BfYmlFhbeUOhIZy%2FXMg22Bj2LIedcL803efbQ%3D'
+    const uri = 'https://starwars5e.table.core.windows.net'
+    const tableService = azure.createTableServiceWithSas(uri, sas)
+    const tableQuery = new azure.TableQuery().top(200)
+    tableService.queryEntities('classes', tableQuery, null, (error, results) => {
+      if (!error) {
+        this.msg = 'Classes: ' + results.entries.map(({Name}) => Name._).join(', ')
+      }
+    })
   }
 }
 </script>
+
+<template>
+  <div class="hello">
+    <h1>Star Wars</h1>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
@@ -107,6 +44,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: red;
+  color: #42b983;
 }
 </style>

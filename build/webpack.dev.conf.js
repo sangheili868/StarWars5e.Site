513658b21f4fe719cf448e41922e2372dee989cf
Alexander Reeder <alexgreeder@LIVE.COM>
2019-04-14 01:32:40 -0400

    More secret management.

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -45,9 +45,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     }
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': require('../config/dev.env')
-    }),
+    new webpack.EnvironmentPlugin(['NODE_ENV', 'tablestoragesas', 'tablestorageurl']),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
     new webpack.NoEmitOnErrorsPlugin(),

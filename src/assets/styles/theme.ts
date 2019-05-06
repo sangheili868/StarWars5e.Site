301ff09992933eb6f96e8de06d70c32f49542436
Maheu <chrisii868@gmail.com>
2019-05-06 15:25:03 -0500

    Improve home page

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,5 +1,10 @@
 export default {
   primary: '#c40f0f',
   secondary: '#0d99cc',
-  background: '#afc6d6'
+  background: '#afc6d6',
+  // Brands
+  Reddit: '#ff4500',
+  Discord: '#7289da',
+  Twitter: '#1da1f2',
+  Patreon: '#f96854'
 }
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -9,68 +9,142 @@
     }
   })
   export default class HomePage extends Vue {
+    get socialLinks () {
+      return [
+        {
+          href: 'https://www.reddit.com/r/sw5e',
+          icon: 'fab fa-reddit',
+          title: 'Reddit'
+        },
+        {
+          href: 'https://discord.gg/zYcPYTu',
+          icon: 'fab fa-discord',
+          title: 'Discord'
+        },
+        {
+          href: 'https://twitter.com/Galiphile',
+          icon: 'fab fa-twitter',
+          title: 'Twitter'
+        },
+        {
+          href: 'https://www.patreon.com/sw5e',
+          icon: 'fab fa-patreon',
+          title: 'Patreon'
+        }
+      ]
+    }
     get categories () {
       return [
         {
-          category: 'Handbook',
+          category: 'Books',
           routes: [
             {
-              title: 'Introduction',
-              to: '/handbook'
+              href: 'https://drive.google.com/file/d/1bVVk719__eeJC388VXV6KXqwRjUOQJ0m/view?usp=sharing',
+              title: 'Player\' Handbook'
             },
             {
-              title: 'Species',
-              to: '/handbook/species'
+              href: '',
+              title: 'Monster Manual'
             },
             {
-              title: 'Classes',
-              to: '/handbook/classes'
-            },
-            {
-              title: 'Combat',
-              to: '/handbook/combat'
+              href: '',
+              title: 'Starships of the Galaxy'
             }
           ]
         },
         {
-          category: 'Starships',
+          category: 'Expanded Content',
           routes: [
             {
-              title: 'Introduction',
-              to: '/starships'
+              href: '',
+              title: 'Species'
             },
             {
-              title: 'Deployments',
-              to: '/starships/deployments'
+              href: '',
+              title: 'Archetypes'
             },
             {
-              title: 'Ships',
-              to: '/starships/starshipSizes'
+              href: '',
+              title: 'Backgroudns'
             },
             {
-              title: 'Combat',
-              to: '/starships/combat'
-            }
-          ]
-        },
-        {
-          category: 'Reference',
-          routes: [
-            {
-              to: '/reference/weapons',
-              title: 'Weapons'
+              href: '',
+              title: 'Equipment'
             },
             {
-              to: '/reference/forcePowers',
+              href: '',
+              title: 'Customization Options'
+            },
+            {
+              href: '',
               title: 'Force Powers'
             },
             {
-              to: '/reference/techPowers',
+              href: '',
               title: 'Tech Powers'
             },
             {
-              to: '/reference/monsters',
-              title: 'Monsters'
+              href: '',
+              title: 'Variant Rules'
+            }
+          ]
+        },
+        {
+          category: 'Special Equipment',
+          routes: [
+            {
+              href: '',
+              title: 'Enhanced Items'
+            },
+            {
+              href: '',
+              title: 'Cybernetic Augmentations'
+            },
+            {
+              href: '',
+              title: 'Droid Customization'
+            },
+            {
+              href: '',
+              title: 'Modifiable Items'
+            }
+          ]
+        },
+        {
+          category: 'Character Sheets',
+          routes: [
+            {
+              href: '',
+              title: 'Character Sheet'
+            },
+            {
+              href: '',
+              title: 'Character Sheet: Form Fillable'
+            },
+            {
+              href: '',
+              title: 'Pre-generated Characters',
+              author: '/u/papasmurf008'
+            },
+            {
+              href: '',
+              title: 'More Pre-generated Characters (level 5)',
+              author: '/u/cpthogie'
+            }
+          ]
+        },
+        {
+          category: 'Deployment and Starship Sheets',
+          routes: [
+            {
+              href: '',
+              title: 'Deployment Sheet',
+              author: '/u/speedreeder'
+            },
+            {
+              href: '',
+              title: 'Starship Sheet',
+              author: '/u/speedreeder'
             }
           ]
         }
@@ -82,5 +156,24 @@
 <template lang="pug">
   div
     h1 Star Wars 5e
-    RoutesList(:categories="categories")
+    div(:class="$style.routes").mb-5
+      a(v-for="{ href, icon, title, author } in socialLinks", :key="title", :href="href", target="_blank")
+        v-btn
+          v-icon(:color="title").mr-3 {{ icon }}
+          | {{ title }}
+    div(v-for="{ category, routes } in categories", :key="category").mb-5
+      h3 {{ category }}
+      div(:class="$style.routes")
+        div(v-for="{ href, title, author } in routes", :key="title" )
+          a(:href="href", target="_blank")
+            v-btn(color="primary") {{ title }}
+          div(v-if="author") courtesy of {{ author}}
 </template>
+
+<style module lang="scss">
+  .routes {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>

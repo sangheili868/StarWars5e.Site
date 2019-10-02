25a050f1925c66edb0efa6e38ac91fe630b7371a
Maheu <chrisii868@gmail.com>
2019-10-02 13:57:46 -0500

    Add list of character choices

--- src/pages/MyContent/CharacterBuilder/CharacterBuilder.vue
+++ src/pages/MyContent/CharacterBuilder/CharacterBuilder.vue
@@ -12,12 +12,13 @@
     h1 Character Builder
     div Coming Soon!
     ol.text-left
-      li Basics
+      li Settings
         ul
-          li Name
-          li Image
           li Username
-          li Level/Experience Points
+          li Fixed or Roll hit points
+          li Ability Score Method (Manual, Standard Set, Point Buy)
+          li Defense Rolls?
+          li Saving Throw Checks?
       li Species
         ul
           li Species
@@ -28,7 +29,50 @@
           li * Weapon proficiency
           li * Language
       li Class
+        ul
+          li Class
+          li is starting class
+          li Levels
+          li Hit Points
+          li * Tool proficiency
+          li Skills
+          li Ability Score Improvements
+          li * Fighting Style
+          li * Maneuvers
+          li * Tech Powers
+          li * Force Powers
+          li * Expertise
+          li * Discoveries (Scholar)
+          li * Empowerment Options (Consular)
+          li * Sentinel Ideals (Sentinel)
+          li * Guardian Aura (Guardian)
+          li * Archetype
+            ul
+              li * Skill Proficiencies
+              li * Fighting Style
+              li * Languages
+              li * Tech Powers
+              li * Force Powers
+              li * Maneuvers
+              ii * Silver Tongue (Politician Pursuit)
+              li * Cybernetic Enhancements (Augmented Approach)
+              li * Modifications (Engineer)
+              li * Droid Companion (Astrotech Engineer)
+              li * Echani Weapons (Echani Order)
+              li * Hunter Features (Hunter Technique)
       li Ability Scores
       li Description
+        ul
+          li Name
+          li Image
+          li Alignment
+          li Background
+            ul
+              li Skills
+              li * Tool Proficiencies
+              li * Languages
+              li * Feat
+          li Characteristics
       li Equipment
+        li Starting Equipment or Wealth
 </template>

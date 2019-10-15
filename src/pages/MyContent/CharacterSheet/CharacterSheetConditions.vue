972402bd81f7e8a732d83d15dec8bf33b0b11fe4
Maheu <chrisii868@gmail.com>
2019-10-15 14:52:58 -0500

    Start conditions

---
+++ src/pages/MyContent/CharacterSheet/CharacterSheetConditions.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator'
+
+  @Component
+  export default class CharacterSheetConditions extends Vue {
+
+  }
+</script>
+
+<template lang="pug">
+  div TO DO
+</template>
+
+<style module lang="scss">
+
+</style>

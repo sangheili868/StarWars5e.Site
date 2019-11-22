290da951f0a05c18d8f06a6afe03a3b2a5e184f9
Maheu <cmaheu2@dxc.com>
2019-11-22 10:00:59 -0600

    Add step 0 with beta description

--- src/pages/MyContent/CharacterBuilder/CharacterBuilder.vue
+++ src/pages/MyContent/CharacterBuilder/CharacterBuilder.vue
@@ -48,7 +48,7 @@
     @speciesModule.State species!: SpeciesType[]
     @speciesModule.Action fetchSpecies!: () => void
 
-    currentStep = 1
+    currentStep = 0
     isReady = false
 
     created () {
@@ -111,7 +111,7 @@
     }
 
     prevStep () {
-      this.currentStep = Math.max(this.currentStep - 1, 1)
+      this.currentStep = Math.max(this.currentStep - 1, 0)
       window.scrollTo(0, 0)
     }
   }
@@ -119,8 +119,26 @@
 
 <template lang="pug">
   div
-    h1.pb-3 Character Builder
-    v-stepper(v-if="isReady", v-model="currentStep", alt-labels)
+    h1.pb-3 Character Builder (BETA)
+    div(v-if="currentStep === 0")
+      div This character builder is in #[span.primary--text beta]. We are still working on lots of cool options and features!
+      div The next big step is letting you create an account to store characters on our servers.
+      div.
+        Until then, you will have to save your character to a
+        #[span.primary--text file on your computer],
+        and upload it to this site whenever you want to view it
+      div #[span.primary--text Nothing is saved] unless you download the file, including hit points, force points, etc.
+      div.
+        Please report any bugs you find, or features you want to see, at the
+        #[span.primary--text #website-character-builder]
+        channel in our discord server.
+      div.mt-5.d-flex.flex-wrap.justify-space-around
+        a(href="https://discord.gg/zYcPYTu", target="_blank").mb-5
+          v-btn(light)
+            v-icon(color="Discord").mr-3 fab fa-discord
+            | Go to Discord Server
+        v-btn(color="primary", @click="currentStep = 1") Go to character builder
+    v-stepper(v-else-if="isReady", v-model="currentStep", alt-labels)
       v-stepper-header
         template(v-for="n in numSteps")
           v-stepper-step(:key="`${n}-step`", :complete="currentStep > n", :step="n", editable) {{ steps[n].name }}
@@ -134,7 +152,7 @@
           )
           v-btn(v-if="currentStep < numSteps", color="primary", @click="nextStep") Continue
           v-btn(v-if="currentStep === numSteps", color="primary", to="characterSheet") Save and View My Character
-          v-btn(v-if="currentStep > 1", text, @click="prevStep") Back
+          v-btn(v-if="currentStep > 0", text, @click="prevStep") Back
     Loading(v-else)
     div.mt-5 {{ character }}
 

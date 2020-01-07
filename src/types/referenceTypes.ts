be5dbeddbbfd703ee95b043fee409cb007205d67
Nathan J Murphy <nmurphy@turner-industries.com>
2020-01-07 13:38:55 -0600

    feat(props): added properties for flavorText & sectionText - linter didn't like the format (can revert if necessary)

--- src/types/referenceTypes.ts
+++ src/types/referenceTypes.ts
@@ -1,66 +1,68 @@
 export interface MonsterType {
-  alignment: string,
-  armorClass: number,
-  armorType: string,
+  alignment: string;
+  armorClass: number;
+  armorType: string;
+  flavorText: string;
+  sectionText: string;
   behaviors: {
-    name: string,
-    monsterBehaviorType: string,
-    description: string,
-    attackType: string,
-    restrictions: string,
-    attackBonus: number,
-    range: string,
-    numberOfTargets: string,
-    damage: string,
-    damageType: string
-  }[],
-  challengeRating: number,
-  charisma: number,
-  charismaModifier: number,
-  conditionsImmunitiesOther: string[],
-  conditionsImmunitiesParsed: number[],
-  constitution: number,
-  constitutionModifier: number,
-  contentType: string,
-  damageImmunitiesOther: string[],
-  damageImmunitiesParsed: number[],
-  damageResistancesOther: string[],
-  damageResistancesParsed: number[],
-  damageVulnerabilitiesOther: string[],
-  damageVulnerabilitiesParsed: number[],
-  dexterity: number,
-  dexterityModifier: number,
-  experiencePoints: number,
-  hitPointRoll: string,
-  hitPoints: number,
-  intelligence: number,
-  intelligenceModifier: number,
-  languages: string[],
-  name: string,
-  savingThrows: string[],
-  senses: string[],
-  size: string,
-  skills: string[],
-  speed: number,
-  speeds: string,
-  strength: number,
-  strengthModifier: number,
-  types: string[],
-  wisdom: number,
-  wisdomModifier: number
+    name: string;
+    monsterBehaviorType: string;
+    description: string;
+    attackType: string;
+    restrictions: string;
+    attackBonus: number;
+    range: string;
+    numberOfTargets: string;
+    damage: string;
+    damageType: string;
+  }[];
+  challengeRating: number;
+  charisma: number;
+  charismaModifier: number;
+  conditionsImmunitiesOther: string[];
+  conditionsImmunitiesParsed: number[];
+  constitution: number;
+  constitutionModifier: number;
+  contentType: string;
+  damageImmunitiesOther: string[];
+  damageImmunitiesParsed: number[];
+  damageResistancesOther: string[];
+  damageResistancesParsed: number[];
+  damageVulnerabilitiesOther: string[];
+  damageVulnerabilitiesParsed: number[];
+  dexterity: number;
+  dexterityModifier: number;
+  experiencePoints: number;
+  hitPointRoll: string;
+  hitPoints: number;
+  intelligence: number;
+  intelligenceModifier: number;
+  languages: string[];
+  name: string;
+  savingThrows: string[];
+  senses: string[];
+  size: string;
+  skills: string[];
+  speed: number;
+  speeds: string;
+  strength: number;
+  strengthModifier: number;
+  types: string[];
+  wisdom: number;
+  wisdomModifier: number;
 }
 
 export interface VariantRuleBlobType {
-  chapterName: string,
-  contentMarkdown: string
+  chapterName: string;
+  contentMarkdown: string;
 }
 
 export interface SkillsType {
-  [ ability: string ]: string[]
+  [ability: string]: string[];
 }
 
 export interface gdFeats {
-  name: string,
-  description: string,
-  proficiencies: string[]
+  name: string;
+  description: string;
+  proficiencies: string[];
 }

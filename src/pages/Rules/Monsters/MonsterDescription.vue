5c216d5de7389cb37a81e0eb68dd876462c5dc3e
Nathan J Murphy <nmurphy@turner-industries.com>
2020-01-07 13:41:00 -0600

    feat(ui-flavorText): added elements for text. linter seems to have gone bonkers though

--- src/pages/Rules/Monsters/MonsterDescription.vue
+++ src/pages/Rules/Monsters/MonsterDescription.vue
@@ -1,53 +1,89 @@
 <script lang="ts">
-  import { Component, Prop, Vue } from 'vue-property-decorator'
-  import { MonsterType } from '@/types/referenceTypes.ts'
-  import VueMarkdown from 'vue-markdown'
-  import Loading from '@/components/Loading.vue'
-  import _ from 'lodash'
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { MonsterType } from "@/types/referenceTypes.ts";
+import VueMarkdown from "vue-markdown";
+import Loading from "@/components/Loading.vue";
+import _ from "lodash";
 
-  @Component({
-    components: {
-      VueMarkdown,
-      Loading
-    }
-  })
-  export default class MonsterDescription extends Vue {
-    @Prop(Object) readonly monster!: MonsterType
-    @Prop(Boolean) readonly isAtDetail!: boolean
-
-    get abilityScores () {
-      return this.monster && [
-        { score: 'STR', value: this.monster.strength, modifier: this.addPlus(this.monster.strengthModifier) },
-        { score: 'DEX', value: this.monster.dexterity, modifier: this.addPlus(this.monster.dexterityModifier) },
-        { score: 'CON', value: this.monster.constitution, modifier: this.addPlus(this.monster.constitutionModifier) },
-        { score: 'INT', value: this.monster.intelligence, modifier: this.addPlus(this.monster.intelligenceModifier) },
-        { score: 'WIS', value: this.monster.wisdom, modifier: this.addPlus(this.monster.wisdomModifier) },
-        { score: 'CHA', value: this.monster.charisma, modifier: this.addPlus(this.monster.charismaModifier) }
-      ]
-    }
-
-    behaviors (type: string) {
-      return this.monster && this.monster.behaviors.filter(({ monsterBehaviorType }) => monsterBehaviorType === type)
-    }
-
-    addPlus (value: number) {
-      return value >= 0 ? '+' + value : value.toString()
-    }
-
-    hasListOrOther (field: string) {
-      return this.monster && (
-        (this.monster as any)[field].length ||
-        (this.monster as any)[field + 'Other']
-      )
-    }
-
-    joinWithOther (field: string) {
-      return [
-        ...(this.monster && (this.monster as any)[field]) || [],
-        ...(this.monster && (this.monster as any)[field + 'Other']) || []
-      ].join(', ')
-    }
+@Component({
+  components: {
+    VueMarkdown,
+    Loading
   }
+})
+export default class MonsterDescription extends Vue {
+  @Prop(Object) readonly monster!: MonsterType;
+  @Prop(Boolean) readonly isAtDetail!: boolean;
+
+  get abilityScores() {
+    return (
+      this.monster && [
+        {
+          score: "STR",
+          value: this.monster.strength,
+          modifier: this.addPlus(this.monster.strengthModifier)
+        },
+        {
+          score: "DEX",
+          value: this.monster.dexterity,
+          modifier: this.addPlus(this.monster.dexterityModifier)
+        },
+        {
+          score: "CON",
+          value: this.monster.constitution,
+          modifier: this.addPlus(this.monster.constitutionModifier)
+        },
+        {
+          score: "INT",
+          value: this.monster.intelligence,
+          modifier: this.addPlus(this.monster.intelligenceModifier)
+        },
+        {
+          score: "WIS",
+          value: this.monster.wisdom,
+          modifier: this.addPlus(this.monster.wisdomModifier)
+        },
+        {
+          score: "CHA",
+          value: this.monster.charisma,
+          modifier: this.addPlus(this.monster.charismaModifier)
+        }
+      ]
+    );
+  }
+
+  get isDark() {
+    return this.$vuetify.theme.dark;
+  }
+
+  behaviors(type: string) {
+    return (
+      this.monster &&
+      this.monster.behaviors.filter(
+        ({ monsterBehaviorType }) => monsterBehaviorType === type
+      )
+    );
+  }
+
+  addPlus(value: number) {
+    return value >= 0 ? "+" + value : value.toString();
+  }
+
+  hasListOrOther(field: string) {
+    return (
+      this.monster &&
+      ((this.monster as any)[field].length ||
+        (this.monster as any)[field + "Other"])
+    );
+  }
+
+  joinWithOther(field: string) {
+    return [
+      ...((this.monster && (this.monster as any)[field]) || []),
+      ...((this.monster && (this.monster as any)[field + "Other"]) || [])
+    ].join(", ");
+  }
+}
 </script>
 
 <template lang="pug">
@@ -57,6 +93,9 @@
         v-btn(v-if="!isAtDetail", text, icon, color="secondary")
           v-icon fa-external-link-alt
     i {{ monster.size }} {{ monster.types.join(', ')}}, {{ monster.alignment }}
+    hr
+    div(:class="[ $style.sectionFlavorText, { [$style.darkSide]: isDark } ]") {{monster.sectionText}}
+    div(:class="[ $style.monsterFlavorText, { [$style.darkSide]: isDark } ]") {{monster.flavorText}}
     hr.mt-2
     div #[strong Armor Class] {{ monster.armorClass }} ({{ monster.armorType }})
     div #[strong Hit Points] {{ monster.hitPoints }} ({{ monster.hitPointRoll }})
@@ -106,8 +145,40 @@
 </template>
 
 <style module lang="scss">
-  .abilityScores {
-    display: flex;
-    justify-content: flex-start;
+.abilityScores {
+  display: flex;
+  justify-content: flex-start;
+}
+
+.sectionFlavorText {
+  display: flex;
+  margin-left: 48px;
+  margin-right: 48px;
+  margin-top: 12px;
+  margin-bottom: 12px;
+  justify-content: flex-start;
+  font-style: italic;
+
+  border-top: 1px black solid;
+
+  &.darkSide {
+    border-top: 1px white solid;
   }
+}
+
+.monsterFlavorText {
+  display: flex;
+  margin-left: 50px;
+  margin-right: 50px;
+  margin-top: 12px;
+  margin-bottom: 12px;
+  justify-content: flex-start;
+  font-style: italic;
+
+  border-bottom: 1px black solid;
+
+  &.darkSide {
+    border-bottom: 1px white solid;
+  }
+}
 </style>
